3c71556d37680f9b1ab8fc569d985c0954a3186f
KangJeongHyun <kimbab60@naver.com>
2016-12-01 10:09:22 +0900

    modify >> Hello.java : 0278944 by kang

--- Hello_JSP/src/com/test/hello/Hello.java
+++ Hello_JSP/src/com/test/hello/Hello.java
@@ -18,6 +18,11 @@ public class Hello {
 		// 0412518 : origin/master edit by kim
 		System.out.println("GitHub edit 4567");
 		// 0412518 : end
+		
+		// 0278944 : pull by kang
+		System.out.println("GitHub edit 4567891");
+		// 0278944 : end
+				  
 	}
 
 }
