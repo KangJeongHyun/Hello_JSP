2463dd4bec605ce1936b5e679534379ebbfd4470
KangJeongHyun <kimbab60@daum.net>
2016-12-01 09:10:15 +0900

    modify >> Helloe.java : 031524 by kang

--- Hello_JSP/src/com/test/hello/Hello.java
+++ Hello_JSP/src/com/test/hello/Hello.java
@@ -10,6 +10,10 @@ public class Hello {
 		
 		// 0215238 : origin/master edit by kang
 		System.out.println("GitHub edit");
+		
+		// 031524 : local/master edit my kang
+		System.out.println("local edit 1234");
+		// 031524 : end
 	}
 
 }
